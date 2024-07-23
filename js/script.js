be4575e586b469d27d89478c0adb9debc83c3f91
Andrelegrand <andremabutwa1@gmail.com>
2024-07-23 13:39:00 +0200

    Premier commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*============================================ typing animation =================================================*/
+var typed = new Typed(".typing",{
+     strings: ["","Web Developer","Mobile Developer","Data Analyst","Expert Meal"],
+     typeSpeed:100,
+     BackSpeed:60,
+     loop:true
+})
